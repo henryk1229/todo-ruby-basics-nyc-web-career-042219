c47fc323c58934d94469373d1806b903ebeba14c
Henry Koehler <henryk1229@gmail.com>
2019-03-29 14:31:55 +0000

    Automatically backed up by Learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -15,13 +15,13 @@ def greeting(arg1, arg2)
 end
 
 def return_a_value
-
+  "Nice"
 end
 
 def last_evaluated_value
-
+  "expert"
 end
 
-def pizza_party
-
+def pizza_party(argument = "cheese")
+  argument
 end
